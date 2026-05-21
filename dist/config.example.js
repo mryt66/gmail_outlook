786767459f413c3ad0e678c77251467a3be80313
mryt66 <marcin.ryt66@gmail.com>
2026-05-21 23:50:18 +0200

    Remove dist from git tracking

--- dist/config.example.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  supabaseUrl: 'https://TWOJ-PROJEKT.supabase.co',
-  supabaseAnonKey: 'TWOJ_PUBLICZNY_ANON_KEY'
-};
--- dist/config.js
+++
@@ -1,4 +0,0 @@
-window.APP_CONFIG = {
-  supabaseUrl: 'https://safjbkbguskradqrpqdr.supabase.co',
-  supabaseAnonKey: 'sb_publishable_uZla1KqWacpwZKdp8d-4ug_I54rI7oP'
-};
